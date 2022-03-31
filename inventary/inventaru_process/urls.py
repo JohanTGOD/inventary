187b7bfbaa3fb8a645f6ca07c36ac84e41544c60
JohanTGOD <johan.desarrollador@gmail.com>
2022-03-31 08:24:37 -0500

    Adding styles

--- inventary/inventaru_process/urls.py
+++ inventary/inventaru_process/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name="inventaryUrl"
 urlpatterns = [
-    path("",views.index, name="index")
+    #path("",views.index, name="index"),
+    path("",views.IndexView.as_view(), name="index")
 ]
--- inventary/inventaru_process/views.py
+++ inventary/inventaru_process/views.py
@@ -1,9 +1,20 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Product,Category,Supplier,StateProduct
+from django.views import generic
 
-def index(request):
-    products=Product.objects.all()
-    return render(request,"inventaru_process/index.html",{
-        "all_products":products
-    })
+
+class IndexView(generic.ListView):
+    template_name="inventaru_process/listproducts.html"
+    context_object_name = "all_products"
+    
+    def get_queryset(self):
+        return Product.objects.order_by("-name")
+
+
+#def index(request):
+#    products=Product.objects.all()
+#    return render(request,"inventaru_process/listproducts.html",{
+#        "all_products":products
+#    })
